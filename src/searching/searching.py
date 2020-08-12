2bcfe772c44fbec43f47bbf932d53f01a3180945
Jonathan Mendoza <jon_94us@yahoo.com>
2020-08-11 21:48:42 -0500

    recursive searching and sorting completed. tests passed

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,22 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
+
+	if start <= end:
+
+		mid = (start + end) // 2
+
+		if arr[mid] == target:
+			return mid
+
+		elif arr[mid] > target:
+
+			return binary_search(arr, target, start, mid - 1)
+
+		else:
+			return binary_search(arr, target, mid+ 1, end)
+	else:
+
+		return -1
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +26,36 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+	
+	start = 0
+	end = len(arr) - 1
 
+	if start != end:
+
+		mid = (start + end) // 2
+
+		if arr[mid] == target:
+			return mid
+
+		elif arr[mid] > target:
+
+			if arr[start] < arr[mid]:
+
+				return agnostic_binary_search(arr[start:(mid - 1)], target)
+
+			else:
+				return agnostic_binary_search(arr, target, mid + 1, start)
+
+		else:
+			if arr[end] > arr[start]:
+
+				return agnostic_binary_search(arr, target, mid+ 1, end)
+
+			else:
+
+				return agnostic_binary_search(arr, target, end, mid-1)
+
+
+	else:
+
+		return -1
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,66 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
-def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+def merge(a, b):
 
-    # Your code here
+	sorted = []
+
+	ai = 0
+	bi = 0
+
+	count = len(a) + len(b)
+
+	while len(sorted) < count:
+
+		if ai >= len(a):
+
+			sorted.append(b[bi])
+			bi += 1
+
+		elif bi>= len(b):
+
+			sorted.append(a[ai])
+			ai += 1
 
 
-    return merged_arr
+		elif a[ai] < b[bi]:
+
+			sorted.append(a[ai])
+			ai += 1
+
+		elif a[ai] >= b[bi]:
+
+			sorted.append(b[bi])
+			bi += 1
+
+	return sorted
+
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
+
+	if len(arr) < 2:
+		return arr
+
+	#Base Case
+	elif len(arr) > 1:
+
+		mid = len(arr)//2
+
+		LHS = arr[:mid]
+		RHS = arr[mid:]
+
+		LHS = merge_sort(LHS)
+		RHS = merge_sort(RHS)
+
+		return merge(LHS,RHS)
 
 
-    return arr
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
-    # Your code here
+	pass
 
 
 def merge_sort_in_place(arr, l, r):
-    # Your code here
-
+	pass
